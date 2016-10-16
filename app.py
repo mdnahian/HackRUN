7330dad4295547bad35bb406e24f0878bca1168f
Md Islam <mdnahian@outlook.com>
2016-10-16 21:30:01 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(
+		host="0.0.0.0",
+		port=int("8800"),
+		debug=True
+	)
